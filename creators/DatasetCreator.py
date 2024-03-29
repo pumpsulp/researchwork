ab14e50e53f7724726fd3513c26fc2997224093b
Mortuus Stellaris <pumpsulp@gmail.com>
2024-03-29 11:05:53 +0400

    релизовал логику создания набора данных

---
+++ creators/DatasetCreator.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+from torch.utils.data import Dataset
+
+from object.DataSplit import DataSplit
+from object.ObjectStorage import ObjectStorage
+from object.ObjectDataset import DatasetElement, ObjectDataset
+
+
+@dataclass
+class DatasetCreator(ABC):
+    """Реализует различные варианты создания набора данных (датасета)"""
+    
+    @abstractmethod
+    def create_dataset(self, data) -> Dataset:
+        raise NotImplementedError("Subclasses of TypeDataset should implement method create_dataset.")
+
+
+@dataclass
+class ObjectDatasetCreator(DatasetCreator):
+    """Реализует создание набора данных из ObjectStorage"""
+    splitter: DataSplit = field(default=None)
+    
+    def create_dataset(self, object_storage: ObjectStorage) -> Dataset | tuple[Dataset]:
+        data = []
+        
+        labels = {cls: label for label, cls in enumerate(object_storage.get_objects_classes())}
+        
+        for obj in object_storage.objects:
+            for photo in obj.photos:
+                image = photo.get_image()
+                data.append(DatasetElement(Image=image, Label=labels[obj.name]))
+        
+        dataset = ObjectDataset(data=data)
+        
+        if self.splitter is not None:
+            dataset = self.splitter.split(dataset)
+        
+        return dataset
---
+++ object/DataSplit.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+
+from numpy.random import RandomState
+from sklearn.model_selection import train_test_split
+
+
+@dataclass
+class DataSplit:
+    """Реализует разбиение набора данных на выборки: тренировочную, валидационную (опционально), тестовую.
+    Инициализируемые параметры:
+    test_size: если float - то доля от общей выборки, если int - конкретное число элементов выборки.
+    Если не указан,
+    valid_size: аналогично test_size, но доля от обучающей выборки
+    random_state: seed
+    shuffle: флаг, отвечающий за перемешивание общей выборки"""
+    
+    test_size: float | int = field(default=None)
+    valid_size: float | int = field(default=None)
+    random_state: int | RandomState = field(default=None)
+    shuffle: bool = field(default=True)
+    
+    def split(self, data):
+        train_data, test_data = train_test_split(data,
+                                                 test_size=self.test_size,
+                                                 random_state=self.random_state,
+                                                 shuffle=self.shuffle)
+        if self.valid_size is not None:
+            train_data, valid_data = train_test_split(train_data,
+                                                      test_size=self.valid_size,
+                                                      random_state=self.random_state,
+                                                      shuffle=self.shuffle)
+            return train_data, valid_data, test_data
+        else:
+            return train_data, test_data
