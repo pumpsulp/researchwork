943123f21276282814b1b99584149031186dfb60
Mortuus Stellaris <pumpsulp@gmail.com>
2024-04-20 14:10:02 +0400

    небольшие изменения

--- creators/DatasetCreator.py
+++ creators/DatasetCreator.py
@@ -9,7 +9,7 @@ from objects.ObjectDataset import DatasetElement, ObjectDataset
 from objects.ObjectStorage import ObjectStorage
 from utils.DataSplit import DataSplit, ObjectDataSplit
 
-DatasetWithLoader = namedtuple('DatasetWithLoader', ['dataset', 'dataloader'])
+DatasetWithLoader = namedtuple('DatasetWithLoader', ['data', 'dataloader'])
 
 
 @dataclass
@@ -17,17 +17,22 @@ class DatasetCreator(ABC):
     """Реализует различные варианты создания набора данных (датасета)"""
     
     @abstractmethod
-    def create_dataset(self, data) -> Dataset:
+    def create_dataset(self, *args, **kwargs) -> Dataset:
         raise NotImplementedError("Subclasses of TypeDataset should implement method create_dataset.")
 
 
 @dataclass
 class ObjectDatasetCreator(DatasetCreator):
     """Реализует создание набора данных из ObjectStorage"""
-    batch_size: int
-    splitter: ObjectDataSplit
     
-    def create_dataset(self, object_storage: ObjectStorage) -> dict[str: DatasetWithLoader]:
+    @classmethod
+    def create_dataset(cls, object_storage: ObjectStorage,
+                       batch_size: int,
+                       test_size: float,
+                       valid_size: float = None) -> dict[str: DatasetWithLoader]:
+        
+        splitter = ObjectDataSplit(test_size=test_size, valid_size=valid_size)
+        
         data = []
         
         labels = {cls: label for label, cls in enumerate(object_storage.get_objects_classes())}
@@ -38,14 +43,14 @@ class ObjectDatasetCreator(DatasetCreator):
                 data.append(DatasetElement(Image=image, Label=labels[obj.name]))
         
         # Разделение выборки на train, val (optional), test
-        split_dataset = self.splitter.split(ObjectDataset(data=data))
+        split_dataset = splitter.split(ObjectDataset(data=data))
         
-        if self.splitter.valid_size is not None:
-            abbr = ['train', 'val', 'test']
+        if splitter.valid_size is not None:
+            abbr = ['train', 'valid', 'test']
         else:
             abbr = ['train', 'test']
         
-        dataset = {abbr: DatasetWithLoader(data, DataLoader(data, batch_size=self.batch_size))
+        dataset = {abbr: DatasetWithLoader(data, DataLoader(data, batch_size=batch_size))
                    for abbr, data in zip(abbr, split_dataset)}
         
         return dataset
