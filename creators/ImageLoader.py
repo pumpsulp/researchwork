f862098e557456baf333ba3b16266e8657b48e3b
Mortuus Stellaris <pumpsulp@gmail.com>
2024-04-20 13:58:11 +0400

    рефакторинг

--- creators/ImageLoader.py
+++ creators/ImageLoader.py
@@ -4,21 +4,13 @@ from pathlib import Path
 from PIL import Image
 
 
-class ImageLoader(ABC):
+class ImageLoader:
     """Реализует загрузку изображений разных форматов"""
     
-    @abstractmethod
-    def load_image(self, path: Path) -> Image.Image:
-        raise NotImplementedError("Subclasses of TypeImageLoader should implement method load_image.")
-    
-    @abstractmethod
-    def load_images(self, path: Path) -> list[Image.Image]:
-        raise NotImplementedError("Subclasses of TypeImageLoader should implement method load_images.")
-
-
-class PngImageLoader(ImageLoader):
-    def load_image(self, path: Path) -> Image.Image:
+    @classmethod
+    def load_image(cls, path: Path) -> Image.Image:
         return Image.open(path).convert('RGB')
     
-    def load_images(self, path: Path) -> list[Image.Image]:
-        return [self.load_image(image) for image in path.rglob('*.png')]
+    @classmethod
+    def load_images(cls, path: Path, img_format: str) -> list[Image.Image]:
+        return [cls.load_image(image) for image in path.rglob(f'*.{img_format}')]
