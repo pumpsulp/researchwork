cd625192594db763e28e0ba892ba6ac57ed095ff
Mortuus Stellaris <pumpsulp@gmail.com>
2024-04-22 16:11:14 +0400

    добавлена нормализация

--- transform/Transform.py
+++ transform/Transform.py
@@ -28,27 +28,36 @@ class ToTensor(Transform):
         return SampleUnit(torch.tensor(image), torch.tensor(label))
 
 
-class Rescale(Transform):
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
+class Normalize(Transform):
+    def __call__(self, sample: SampleUnit):
+        image = sample.image
+        image /= 255
+        return SampleUnit(image, sample.label)
+        
     
-    def __call__(self, sample):
-        image, label = sample.image, sample.label
         
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * w / h
-        else:
-            new_h, new_w = self.output_size
-        
-        new_h, new_w = int(new_h), int(new_w)
-        
-        img = transform.resize(image, (new_h, new_w))
-        
-        return SampleUnit(img, label)
+
+# class Rescale(Transform):
+#     def __init__(self, output_size):
+#         assert isinstance(output_size, (int, tuple))
+#         self.output_size = output_size
+#
+#     def __call__(self, sample):
+#         image, label = sample.image, sample.label
+#
+#         h, w = image.shape[:2]
+#         if isinstance(self.output_size, int):
+#             if h > w:
+#                 new_h, new_w = self.output_size * h / w, self.output_size
+#             else:
+#                 new_h, new_w = self.output_size, self.output_size * w / h
+#         else:
+#             new_h, new_w = self.output_size
+#
+#         new_h, new_w = int(new_h), int(new_w)
+#
+#         img = transform.resize(image, (new_h, new_w))
+#
+#         return SampleUnit(img, label)
     
     
