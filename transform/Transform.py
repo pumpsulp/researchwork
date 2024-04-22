bd9fd8de680a0ec63c09810419d0cec2262da0f0
Mortuus Stellaris <pumpsulp@gmail.com>
2024-04-22 15:52:23 +0400

    пофикшен баг с размерностями

--- transform/Transform.py
+++ transform/Transform.py
@@ -22,6 +22,7 @@ class ToTensor(Transform):
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C x H x W
+        
         image = np.asarray(image).transpose((2, 0, 1))
         
         return SampleUnit(torch.tensor(image), torch.tensor(label))
